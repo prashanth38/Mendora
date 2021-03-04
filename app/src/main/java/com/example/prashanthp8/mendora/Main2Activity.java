60e50c4a182988eceab92ca5e1b582e1b78582a4
prashanth38 <prashanthbommidi@gmail.com>
2021-03-04 13:19:43 +0530

    first commit

---
+++ app/src/main/java/com/example/prashanthp8/mendora/Main2Activity.java
@@ -0,0 +1,13 @@
+package com.example.prashanthp8.mendora;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
---
+++ app/src/main/java/com/example/prashanthp8/mendora/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.prashanthp8.mendora;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends ActionBarActivity {
+    Button b1,b2,b3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn = (Button)findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Main2Activity.class));
+            }
+        });
+    }
+}
